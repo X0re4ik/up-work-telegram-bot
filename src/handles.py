ed0231be224149e0050eb3c9c7a4e9e508b0c290
X0re4ik <xoore4ik@gmail.com>
2024-06-06 15:13:13 +0500

    Update heandlers

--- src/handles.py
+++ src/handles.py
@@ -7,12 +7,13 @@ from aiogram.filters.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.redis import RedisStorage
 from aiogram.types import Message
+from aiogram import types
 
 from config.configs import redis_config
 
 from .messages import COMMANDS, MESSAGE
 from .services import telegram_user_service
-from .utils import is_authorized, error_handler
+from .utils import is_authorized, error_handler, questions_to_html
 
 
 class RegistrationState(StatesGroup):
@@ -27,6 +28,9 @@ class RegistrationState(StatesGroup):
 class QuestionState(StatesGroup):
     START_WORKED = State()
 
+
+class QuestionDeleteState(StatesGroup):
+    START_WORKED = State()
 
 storage = RedisStorage.from_url(redis_config.URL)
 dp = Dispatcher(storage=storage)
@@ -67,14 +71,7 @@ async def command_get(message: Message) -> None:
             user_account.org_uid,
             user_account.user_uid,
             user_account.security_token,
-            "".join(
-                list(
-                    map(
-                        lambda item: f"{str(item[0]+1)}. <code>{item[1]}</code>\n",
-                        enumerate(user_account.questions)
-                    )
-                )
-            ),
+            questions_to_html(user_account.questions)
         ),
         parse_mode=default_parse_mode
     )
@@ -119,7 +116,9 @@ async def command_set_query(message: Message, state: FSMContext) -> None:
     numbers_of_questions = await telegram_user_service.get_quantity_questions(telegram_id)
     free_request = numbers_of_questions.free
     await message.answer(
-        "Осталось {free_request} запросов",
+        MESSAGE["NUMBER_QUESTION"].format(
+            free_request
+        ),
         parse_mode=default_parse_mode
     )
     if free_request > 0:
@@ -128,11 +127,6 @@ async def command_set_query(message: Message, state: FSMContext) -> None:
             parse_mode=default_parse_mode
         )
         return await state.set_state(QuestionState.START_WORKED)
-    
-    await message.answer(
-        "Лимит на запросы исчерпан",
-        parse_mode=default_parse_mode
-    )
     return await state.set_state(None)
     
 
@@ -156,17 +150,10 @@ async def command_start_worker(message: Message, state: FSMContext) -> None:
     
     questions = await telegram_user_service.add_question(telegram_id, message.text)
     
-    msg = "".join(
-        list(
-            map(
-                lambda question: f"<code>{question}</code>\n",
-                questions
-            )
-        )
-    )
-    
     await message.answer(
-        text=msg,
+        text=MESSAGE["QUERIES"].format(
+            questions_to_html(questions)     
+        ),
         parse_mode=default_parse_mode
     )
     
@@ -216,12 +203,61 @@ async def command_entered_user_uid(message: Message, state: FSMContext) -> None:
     await state.set_state(RegistrationState.READY_TO_RECORD_CONDITIONS)
     
 
-@dp.message(Command(COMMANDS["RESET_QUERY"]))
+@dp.message(Command(COMMANDS["RESET_QUERIES"]))
 @error_handler
 @is_authorized
 async def command_reset_query(message: Message) -> None:
     await telegram_user_service.delete_all_questions(message.from_user.id)
     await message.answer(
         MESSAGE["RESET_QUERY"],
+        parse_mode=default_parse_mode,
+    )
+    
+
+@dp.message(Command(COMMANDS["DELETE_QUERY"]))
+@error_handler
+@is_authorized
+async def command_delete_query(message: Message, state: FSMContext) -> None:
+    telegram_id = message.from_user.id
+    questions = await telegram_user_service.get_questions(telegram_id)
+    
+    kb = list(
+        map(
+            lambda question: [types.KeyboardButton(text=question)],
+            questions
+        )
+    )
+    
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb, 
+        resize_keyboard=True, 
+        one_time_keyboard=True
+    )
+    
+    await message.answer(
+        MESSAGE["INPUT_DELETE_QUERY"],
+        parse_mode=default_parse_mode,
+        reply_markup=keyboard
+    )
+    await state.set_state(QuestionDeleteState.START_WORKED)
+
+
+@dp.message(QuestionDeleteState.START_WORKED)
+@error_handler
+async def command_delete_query(message: Message, state: FSMContext) -> None:
+    telegram_id = message.from_user.id
+    text = message.text
+    
+    result: bool = await telegram_user_service.delete_questions(telegram_id, text)
+    
+    msg = MESSAGE["SUCCESS_DELETE"].format(text)
+    await state.set_state(None)
+    
+    if not result:
+        msg = MESSAGE["INPUT_ERROR"]
+        await state.set_state(QuestionDeleteState.START_WORKED)  
+    
+    await message.answer(
+        msg,
         parse_mode=default_parse_mode,
     )
